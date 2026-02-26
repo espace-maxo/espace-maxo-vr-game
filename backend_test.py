ebb4241cfb15e07f0013bb96a4103f43b95744ff
Marcel Hounhanou <mhounhanou@espace-maxo.com>
2026-02-26 12:40:55 +0000

    auto-commit for d7e638ad-c3ce-4247-a78d-9b54d35d7b07

--- backend_test.py
+++ backend_test.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime, timedelta
 
 class EspaceMaxoAPITester:
-    def __init__(self, base_url="https://delivery-cotonou.preview.emergentagent.com/api"):
+    def __init__(self, base_url="https://combo-table-booking.preview.emergentagent.com/api"):
         self.base_url = base_url
         self.tests_run = 0
         self.tests_passed = 0
